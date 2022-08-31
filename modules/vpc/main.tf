7891d74e12963dfbc78d2e7b953d4cfb43f9f2e7
William Chrisp <william.chrisp@contino.io>
2022-08-31 11:42:11 +1000

    Add support for different az subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,3 +1,8 @@
+# Pulling available AZs
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 # VPC Configuration
 resource "aws_vpc" "main" {
     cidr_block = var.vpc_cidr
@@ -11,7 +16,8 @@ resource "aws_subnet" "public" {
     count = length(var.public_subnets)
     vpc_id = aws_vpc.main.id
     cidr_block = var.public_subnets[count.index]
-
+    availability_zone = data.aws_availability_zones.available.names[count.index]
+    
     tags = {
         Name = "${var.tags.Name} Public ${count.index}"
         }
@@ -21,6 +27,7 @@ resource "aws_subnet" "private" {
     count = length(var.private_subnets)
     vpc_id = aws_vpc.main.id
     cidr_block = var.private_subnets[count.index]
+    availability_zone = data.aws_availability_zones.available.names[count.index]
 
     tags = {
         Name = "${var.tags.Name} Private ${count.index}"
